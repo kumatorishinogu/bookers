7ae6b23e3e94bc97b669511bd934ca53c2f1e92c
ShinoguKumatori <124132788+kumatorishinogu@users.noreply.github.com>
2023-02-16 11:26:22 +0000

    viewファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  get 'lists/books'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-   get '/top' => 'homes#top'
+  get 'books/top' => 'books#top'  
+   get 'homes/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
